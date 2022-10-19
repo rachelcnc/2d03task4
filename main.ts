accde64949be257f1790faaeda43d61b79af4e89
rachelcnc <115052360+rachelcnc@users.noreply.github.com>
2022-10-19 09:30:28 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.AB, function () {
     xvalue = 0
     while (xvalue <= 5) {
         led.plot(xvalue, 0)
@@ -7,15 +7,26 @@ input.onButtonPressed(Button.A, function () {
         xvalue += 1
     }
 })
-input.onButtonPressed(Button.B, function () {
-    yvalue = 0
-    for (let index = 0; index < 5; index++) {
+let xvalue = 0
+xvalue = 0
+let yvalue = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        led.plot(xvalue, 0)
+        basic.pause(100)
+        xvalue += 1
+    }
+    if (xvalue == 5) {
+        xvalue = 0
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
         led.plot(0, yvalue)
         basic.pause(100)
-        basic.clearScreen()
         yvalue += 1
     }
+    if (yvalue == 5) {
+        yvalue = 0
+    }
 })
-let yvalue = 0
-let xvalue = 0
-basic.showString("2D03 Rachel Chan")
